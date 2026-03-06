72d80de07476aa13ff4c8b797bcf4f3e21d51876
liveapkprox <azzeddineguendouz71@gmail.com>
2026-03-06 01:06:27 +0100

    Update jio.php

--- jio.php
+++ jio.php
@@ -1,8 +1,16 @@
 <?php
-// الرابط الحقيقي للبث
-$real_stream = "https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-1-na/eec141533c90dd34722c503a296dd0d8/index.m3u8";
 
-// تحويل المستخدم للرابط الحقيقي
-header("Location: $real_stream");
+$id = $_GET['id'];
+
+$streams = [
+"mbc1" => "https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-1-na/eec141533c90dd34722c503a296dd0d8/index.m3u8"
+];
+
+if(!isset($streams[$id])){
+ die("Channel not found");
+}
+
+header("Location: ".$streams[$id]);
 exit;
+
 ?>
