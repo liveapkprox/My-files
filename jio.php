c2f84dd868ffdf277f3bba8a282271cd92b3f550
liveapkprox <azzeddineguendouz71@gmail.com>
2026-03-06 01:00:41 +0100

    Update jio.php

--- jio.php
+++ jio.php
@@ -1,4 +1,8 @@
 <?php
-header("Location: https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-1-na/eec141533c90dd34722c503a296dd0d8/index.m3u8");
+// الرابط الحقيقي للبث
+$real_stream = "https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-1-na/eec141533c90dd34722c503a296dd0d8/index.m3u8";
+
+// تحويل المستخدم للرابط الحقيقي
+header("Location: $real_stream");
 exit;
 ?>
