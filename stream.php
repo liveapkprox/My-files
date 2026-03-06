59273fe31315efcb1600932ba86f204198f6b6c9
live <azzeddineguendouz71@gmail.com>
2026-03-06 01:33:58 +0100

    Update stream.php

--- stream.php
+++ stream.php
@@ -7,6 +7,7 @@ header("Content-Type: application/vnd.apple.mpegurl");
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
 $data = curl_exec($ch);
 curl_close($ch);
