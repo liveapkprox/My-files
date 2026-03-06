4bf4b09ec58147787f3af994ef7b0b37791a2561
live <azzeddineguendouz71@gmail.com>
2026-03-06 01:32:45 +0100

    Update stream.php

--- stream.php
+++ stream.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-1-na/eec141533c90dd34722c503a296dd0d8/index.m3u8";
+$url = "https://7seen.xyz/live/7seen/main/40.m3u8";
 
 header("Content-Type: application/vnd.apple.mpegurl");
 
